97ee7059f31b6ae760a1a83bd9847bca1c2c5023
Christopher Walsh <5120876+walshification@users.noreply.github.com>
2026-03-24 09:32:22 -0500

    Move the player

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import pygame
 
 from constants import SCREEN_HEIGHT, SCREEN_WIDTH
 from logger import log_state
+from player import Player
 
 
 def main():
     pygame.init()
     game_clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         log_state()
@@ -16,6 +18,8 @@ def main():
                 return
 
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
         passed = game_clock.tick(60)
         dt = passed / 1000
